4d0c50906b008fe22b1fc510399d557b5857be71
pratama <pratamahelditio@gmail.com>
2023-05-30 23:43:56 +0700

    fix bug extention page

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     BASE_URL_V1: process.env.BASE_URL_V1,
     BASE_URL: process.env.BASE_URL,
   },
-  pageExtensions: ["page.jsx", "page.js", "page.tsx", "page.ts"],
+  pageExtensions: ["tsx"],
 };
 
 module.exports = nextConfig;
