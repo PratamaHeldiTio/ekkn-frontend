fc2bbdfd46f3f3621592288fbc8e102c337bdb5f
pratama <pratamahelditio@gmail.com>
2023-05-31 20:33:23 +0700

    add loading beetwen page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 import { Analytics } from "@vercel/analytics/react";
+import { Loading } from "@/components/Loading";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Ekkn - Trunojoyo</title>
       </Head>
+      <Loading />
       <Component {...pageProps} />
       <Analytics />
     </>
